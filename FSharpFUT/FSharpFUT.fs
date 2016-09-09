936cf8fbbbd62f79a9365d671831ea7aa8acfceb
Peter Sparks HCO <psparks1992@gmail.com>
2016-09-09 14:20:13 -0500

    adding romelu lukaku

--- FSharpFUT/FSharpFUT.fs
+++ FSharpFUT/FSharpFUT.fs
@@ -11,13 +11,14 @@ module FSharpFUT =
         Team: string
     }
 
+    type LinkQuality = Bad | Okay | Good | Perfect
 
     let ComputeLink (player1 : PlayerInfo, player2: PlayerInfo) = 
         match (player1, player2) with
-        | _ when player1.Country = player2.Country && player1.League <> player2.League -> 1
-        | _ when player1.Country = player2.Country && player1.League = player2.League && player1.Team <> player2.Team -> 2
-        | _ when player1.Country = player2.Country && player1.League = player2.League && player1.Team = player2.Team -> 3
-        | _ when player1.Country <> player2.Country && player1.League = player2.League && player1.Team = player2.Team -> 2
-        | _ when player1.Country <> player2.Country && player1.League = player2.League && player1.Team <> player2.Team -> 1
-        | _ -> 0
+        | _ when player1.Country = player2.Country && player1.League <> player2.League -> Okay
+        | _ when player1.Country = player2.Country && player1.League = player2.League && player1.Team <> player2.Team -> Good
+        | _ when player1.Country = player2.Country && player1.League = player2.League && player1.Team = player2.Team -> Perfect
+        | _ when player1.Country <> player2.Country && player1.League = player2.League && player1.Team = player2.Team -> Good
+        | _ when player1.Country <> player2.Country && player1.League = player2.League && player1.Team <> player2.Team -> Okay
+        | _ -> Bad
     
