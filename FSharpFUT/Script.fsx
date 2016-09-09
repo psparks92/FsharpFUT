20d835e99f1749f12378c178cbbe3510a49355d7
Peter Sparks HCO <psparks1992@gmail.com>
2016-09-09 14:20:34 -0500

    adding lukaku

--- FSharpFUT/Script.fsx
+++ FSharpFUT/Script.fsx
@@ -9,8 +9,10 @@ let RobertLewandowski = {Country = "Poland"; League = "Bundesliga"; Team = "Baye
 let MarcoReus = {Country = "Germany"; League = "Bundesliga"; Team = "Dortmund"}
 let PhillipLahm = {Country = "Germany"; League = "Bundesliga"; Team = "Bayern"}
 let EdenHazard = {Country = "Belgium"; League = "PremierLeague"; Team = "Chelsea"}
+let RomeluLukaku = {Country = "Belgium"; League = "PremierLeague"; Team = "Chelsea"}
 
 let RobertMarco = ComputeLink (RobertLewandowski, MarcoReus)
 let PhillipRobert = ComputeLink (RobertLewandowski, PhillipLahm)
 let PhillipMarco = ComputeLink (PhillipLahm, MarcoReus)
 let PhillipEden = ComputeLink (PhillipLahm, EdenHazard)
+let EdenRomelu = ComputeLink (EdenHazard, RomeluLukaku)
