d90b131a8b02e6d46cb7a87d217ab30b7ea1a326
Peter Sparks HCO <psparks1992@gmail.com>
2016-10-05 12:49:04 -0500

    working mongo read

---
+++ FSharpFUTTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace FSharpFUTTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConnectToMongo()
+        {
+            var client = new MongoClient(System.Configuration.ConfigurationManager.ConnectionStrings["local"].ConnectionString);
+            var database = client.GetDatabase("local");
+            var collection = database.GetCollection<BsonDocument>("players");
+            long count = collection.Count(new BsonDocument());
+            Assert.AreNotEqual(0, count);
+        }
+    }
+}
