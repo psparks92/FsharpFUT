2d31d40eeeb56859ea542870ffce1a62140ece65
EC2 Default User <ec2-user@ip-172-31-17-155.us-west-2.compute.internal>
2016-10-27 01:20:30 +0000

    adding cors

--- FSharpCore/Program.cs
+++ FSharpCore/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using System.Collections;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace FSharpFUT.API
 {
@@ -10,24 +12,31 @@ namespace FSharpFUT.API
     {
         public static void Main(string[] args)
         {
+	    //var config = new ConfigurationBuilder()
+		//.SetBasePath(Directory.GetCurrentDirectory())
+		//.AddJsonFile("hosting.json", optional: true)
+		//.Build();
             var host = new WebHostBuilder()
                 .UseKestrel()
+		.UseUrls("http://*:5000")
                 .UseStartup<Program>()
                 .Build();
             host.Run();
-            Console.WriteLine("test");
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         { 
             services.AddTransient<PlayerDAL>();
             services.AddMvc();
+	    services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseMvcWithDefaultRoute();
+	    app.UseCors(builder => builder.WithOrigins("http://*")
+	    .AllowAnyHeader());
         }
     }
 }
